7452f297aebfe8e9dcc152a0705328001e2c84c4
Kaviyarasan <kavigunasekar185@gmail.com>
2026-01-08 11:50:24 +0530

    error fix

--- CurrencyConvertor/script.js
+++ CurrencyConvertor/script.js
@@ -2,13 +2,12 @@ let dropdown = document.querySelectorAll('select');
 let currency = fetch("https://api.frankfurter.dev/v1/currencies")
 .then((cur)=>cur.json())
 .then((mes)=>{dispalycurrency(mes)})
-.catch((err)=>{console.log(err+" tjis is error")});
+.catch((err)=>{console.log(err+" this is error")});
 
 function dispalycurrency(res){
     let countrycode = Object.keys(res);
-   
     for(let i of countrycode){
-        let opt =`<option value="${i}">${i}<option>`;
+        let opt =`<option value="${i}">${i}</option>`;
         dropdown[0].innerHTML +=opt;
         dropdown[1].innerHTML +=opt;
     }
@@ -27,7 +26,7 @@ async function convert(){
         let fr = Number(document.getElementById('input').value);
         let final = mess.rates[to]*fr;
         res.textContent = final.toFixed(2);
-        res.style.fontSize='20px'
+        res.style.fontSize='17px'
         res.style.padding='1px'
     })
     .catch((cat)=>{console.log(cat)});
