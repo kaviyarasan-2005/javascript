d0f2f5f469e2d9b6fe01c3da709194a6eb3ca15f
Kaviyarasan <kavigunasekar185@gmail.com>
2026-01-22 18:01:45 +0530

    todo list storage move to local storage

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -2,17 +2,23 @@ let show = document.getElementById('result');
 let btn = document.getElementById("button");
 let input = document.getElementById("input");
 let todos =[];
+window.onload=()=>{
+    console.log("loaded "+localStorage.getItem('todos'));
+    todos = JSON.parse(localStorage.getItem('todos'))||[];
+    todos.forEach(to => addtodo(to));
+}
+  
 btn.addEventListener("click",function (){
-    
     todos.push(input.value);
     addtodo(input.value);
-    console.log(todos);
+  console.log(todos);
     input.value ='';
 });
 function addtodo(todo){
     let para = document.createElement("p");
     para.innerText = todo;
     show.appendChild(para);
+    localStorage.setItem('todos',JSON.stringify(todos));
     para.addEventListener('click',()=>{
         para.style.textDecoration='line-through';
         remove(todo);
@@ -29,4 +35,5 @@ function remove(todo){
     }
     // console.log(index);
     todos.splice(index,1);
+     localStorage.setItem('todos',JSON.stringify(todos));
 }
